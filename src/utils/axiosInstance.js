a5fd3207da7587fa2199cd27f528c82266d95b29
Apple <apple@Apples-MacBook-Air.local>
2025-01-25 16:38:08 +0530

    baseUrl changed to Porduction url of backend

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 axios.defaults.withCredentials = true;
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8000",
+    // baseURL: "http://localhost:8000",
+    baseURL: "https://swissnote-ass-backend.onrender.com",
 })
 axiosInstance.interceptors.request.use((config) => {
     const token = localStorage.getItem('authToken');
