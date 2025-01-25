99d7110c73cf3c7e082ad0df7ef76ccbae09e021
Apple <apple@Apples-MacBook-Air.local>
2025-01-25 16:56:08 +0530

    fix routing vercel

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/', // Update this if your app is served in a subdirectory
+  plugins: [react()],
 });
